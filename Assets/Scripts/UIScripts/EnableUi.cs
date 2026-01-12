5920a4741c8fff6d9c593c7e5b4cc18d430bb839
Furyofth <hamzashaikhsahab5@gmail.com>
2026-01-12 06:03:41 -0700

    Update EnableUi.cs

--- Assets/Scripts/UIScripts/EnableUi.cs
+++ Assets/Scripts/UIScripts/EnableUi.cs
@@ -3,16 +3,39 @@ using UnityEngine;
 public class EnableUi : MonoBehaviour
 {
     public GameObject UI;
-    public towerHealth towerHealth;
+    public towerHealth[] towerHealths;          // up to 4 towers
     public UpgradeCardSpawner UpgradeCardSpawner;
+
+    void Start()
+    {
+    }
 
     void Update()
     {
-        if (towerHealth.IS_DEAD)
+        if (AllTowersDead())
         {
             UpgradeCardSpawner.GiveRandomUpgrade();
             UI.SetActive(true);
-            towerHealth.IS_DEAD = false;
+
+            // reset so it doesn't trigger every frame
+            foreach (towerHealth tower in towerHealths)
+            {
+                tower.IS_DEAD = false;
+            }
         }
+    }
+
+    bool AllTowersDead()
+    {
+        if (towerHealths == null || towerHealths.Length == 0)
+            return false;
+
+        foreach (towerHealth tower in towerHealths)
+        {
+            if (!tower.IS_DEAD)
+                return false;
+        }
+
+        return true;
     }
 }
